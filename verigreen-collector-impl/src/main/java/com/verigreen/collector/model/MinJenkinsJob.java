54de70a011472a40648a93499eeadb85c96a96b7
soninob <barak.sonino@hp.com>
2015-08-25 17:52:42 +0300

    Adding_new_file

---
+++ verigreen-collector-impl/src/main/java/com/verigreen/collector/model/MinJenkinsJob.java
@@ -0,0 +1,43 @@
+/*******************************************************************************
+ *
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ *******************************************************************************/
+package com.verigreen.collector.model;
+
+public class MinJenkinsJob {
+
+	private String buildNumber;
+	private String jenkinsResult;
+	private String branchName;
+	
+	
+	public String getBuildNumber() {
+		return buildNumber;
+	}
+	public void setBuildNumber(String buildNumber) {
+		this.buildNumber = buildNumber;
+	}
+	public String getJenkinsResult() {
+		return jenkinsResult;
+	}
+	public void setJenkinsResult(String jenkinsResult) {
+		this.jenkinsResult = jenkinsResult;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	
+	
+}
